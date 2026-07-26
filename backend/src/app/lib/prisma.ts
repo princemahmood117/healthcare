e7114dd43e85dc9f6471c08f34e6dd63b3a7882c
iftekhar <princemahmood117@gmail.com>
2026-07-26 23:32:20 +0600

    mailing system implemented

--- backend/src/app/lib/prisma.ts
+++ backend/src/app/lib/prisma.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client";
+
 import { envVerse } from "../../config/env";
 
 
