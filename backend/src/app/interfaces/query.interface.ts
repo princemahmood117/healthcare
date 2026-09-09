9e635045aae26d98c55af26c9df16ea47b2c2585
iftekhar <princemahmood117@gmail.com>
2026-09-10 00:14:57 +0600

    constructor for query builder

---
+++ backend/src/app/interfaces/query.interface.ts
@@ -0,0 +1,26 @@
+
+
+export interface PrismaFindManyArgs {
+    where ? : Record<string, unknown>;
+    include ? : Record<string, unknown>;
+    select ? : Record<string, boolean | Record<string, unknown>>;  // nested object may present
+    orderBy ? : Record<string, unknown> | Record<string, unknown[]>
+    skip ? : number;
+    take ? : number;
+    cursor ? : Record<string, unknown>;
+    distinct ? : string[] | string;
+    [key:string] : unknown;
+}
+
+
+export interface PrismaCountArgs {
+    where ? : Record<string, unknown>;
+    include ? : Record<string, unknown>;
+    select ? : Record<string, boolean | Record<string, unknown>>;  // nested object may present
+    orderBy ? : Record<string, unknown> | Record<string, unknown[]>
+    skip ? : number;
+    take ? : number;
+    cursor ? : Record<string, unknown>;
+    distinct ? : string[] | string;
+    [key:string] : unknown;
+}
--- backend/src/app/utils/QueryBuilder.ts
+++ backend/src/app/utils/QueryBuilder.ts
@@ -1,4 +1,28 @@
+
+
+import { PrismaCountArgs, PrismaFindManyArgs } from "../interfaces/query.interface"
+
+
+//  T = model ; 
 export class QueryBuilder <
 T,
-TWhereInput = Record<string, undefined>
-> {}
+TWhereInput = Record<string, unknown>,   // string = field, unknown = oi field er value
+TInclude = Record<string, unknown>
+
+
+> {
+
+    private query : PrismaFindManyArgs;
+    private countQuery : PrismaCountArgs; 
+    private page : number = 1 ;
+    private limit : number = 10;
+    private skip : number = 0;
+    private sortBy : string = 'createdAt';
+    private sortOrder : "asc" | "desc" = "desc";
+    private selectedFields : Record<string, boolean | undefined>;
+
+
+    constructor() {
+
+    }
+}
